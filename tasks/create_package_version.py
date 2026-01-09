1a68297f6463e681120512c99b128593fbcce15c
Sean <sean@digitaldreams.ai>
2026-01-09 13:43:49 -0700

    Add create_package_version task to cumulusci.yml and implement custom FindReplaceWithFilename transform in create_package_version.py

---
+++ tasks/create_package_version.py
@@ -0,0 +1,15 @@
+from cumulusci.core.source_transforms.transforms import FindReplaceTransform
+from cumulusci.tasks.package import CreatePackageVersion as BaseCreatePackageVersion
+from tasks.deploy import FindReplaceWithFilename
+
+
+class CreatePackageVersion(BaseCreatePackageVersion):
+    """CreatePackageVersion task that extends find_replace to also handle filenames."""
+
+    def _init_options(self, kwargs):
+        super()._init_options(kwargs)
+        
+        # Replace any find_replace transforms with our filename-aware version
+        for i, transform in enumerate(self.transforms):
+            if isinstance(transform, FindReplaceTransform):
+                self.transforms[i] = FindReplaceWithFilename(transform.options)
